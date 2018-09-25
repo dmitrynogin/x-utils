8d1be894f130396afa2d44b78ff1852995327c00
Dmitry Nogin <dmitryn@mail.com>
2018-09-24 17:09:51 -0700

    Define JsonObject

---
+++ X.ComponentModel/JsonObject{T}.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace X.ComponentModel
+{
+    [JsonObject]
+    public abstract class JsonObject<T> : ValueObject<T>
+        where T : JsonObject<T>
+    {
+        public static T FromJson(string json) => JsonConvert.DeserializeObject<T>(json);
+        public string ToJson() => JsonConvert.SerializeObject(this);
+    }
+}
